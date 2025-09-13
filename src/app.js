9c9a3b84077b11eaed5d6e382cdbe5b571ba837b
Leonardo Sousa <leonardopedro31@gmail.com>
2025-09-13 10:06:20 -0300

    Mongoose

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const usersRoutes = require("./routes/usersRoute");
 const app = express();
 
 app.use(express.json());
 
-//GET
-//POST
-//PUT
-//DELETE
-
-
 app.use("/users", usersRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
+mongoose.connect("mongodb://localhost:27017/meu-bolso")
+  .then(() => {
+    console.log(" - Banco conectado com sucesso")
+    app.listen(3000, () => {
+      console.log(" - Servidor rodando na porta 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("NAO FOI POSSIVEL CONECTAR NO BANCO")
+  })
+
+
+
+
+
+
+
+
