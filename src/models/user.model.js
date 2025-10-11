8a090e18194922cd549bb7930dd1ffd65cdb8b9c
Leonardo Sousa <leonardopedro31@gmail.com>
2025-10-11 11:07:50 -0300

    Formatacao user.model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const {toJSON, paginate} = require('./plugins');
+const mongoose = require("mongoose");
+const { toJSON, paginate } = require("./plugins");
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, private: true},
-    role: { type: String, default: 'user' },
-    accounts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Account'}]
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, private: true },
+  role: { type: String, default: "user" },
+  accounts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Account" }],
+});
 
 userSchema.plugin(toJSON);
 userSchema.plugin(paginate);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
